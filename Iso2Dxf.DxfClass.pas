89521e248492884e49347c3d0ae32588814d97a9
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-27 16:30:25 +0100

    Aggiunto costruttore e distruttore e un metodo generico per aggiungere elementi

--- Iso2Dxf.DxfClass.pas
+++ Iso2Dxf.DxfClass.pas
@@ -3,11 +3,38 @@ unit Iso2Dxf.DxfClass;
 
 interface
 
-type
-  TDXFClass = class
+uses
+  System.Classes, System.SysUtils;
 
+type
+  TDxfClass = class
+  private
+    FDxfFile: TstringList;
+
+    procedure AddItem(const Code: Integer; const Value: String);
+  public
+    constructor Create;
+    destructor Destroy;
   end;
 
 implementation
+
+{ TDxfClass }
+
+procedure TDxfClass.AddItem(const Code: Integer; const Value: String);
+begin
+  FDxfFile.Add('  ' + Code.ToString);
+  FDxfFile.Add(Value);
+end;
+
+constructor TDxfClass.Create;
+begin
+  FDxfFile:=TStringList.Create;
+end;
+
+destructor TDxfClass.Destroy;
+begin
+  FDxfFile.Free;
+end;
 
 end.
