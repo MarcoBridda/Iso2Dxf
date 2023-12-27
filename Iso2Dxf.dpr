ab2fcd344c0993aa8ac825761f1366430985484c
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-27 11:33:15 +0100

    Creazione della stringlist per il dxf a creazione di una procedura per inserire i dati

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -11,6 +11,12 @@ var
   Rect: TRect;
   Dxf: TStringList;
 
+procedure AddItemToDXF(const aDxf: TStrings; const Code: Integer; const Value: String);
+begin
+  aDxf.Add('  ' + Code.ToString());
+  aDxf.Add(Value);
+end;
+
 begin
   try
     //Creiamo un rettangolo da 30x20mm e lo salviamo come dxf
@@ -20,6 +26,14 @@ begin
     WriteLn('Width : ', Rect.Width);
     WriteLn('Height: ', Rect.Height);
     WriteLn('Origin: ', Rect.CenterPoint.X,':',Rect.CenterPoint.Y);
+
+    //Creiamo il dxf e scriviamo il rettangolo
+    Dxf:=TStringList.Create();
+    try
+      //Cose da fare...
+    finally
+      Dxf.Free
+    end;
 
     //Fine
     ReadLn
