a2aad6af70b7bca267289249a27550bfd0977ed2
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-27 11:21:25 +0100

    Stampiamo un po' di info (dimensioni e punto centrale)

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -14,6 +14,14 @@ begin
   try
     //Creiamo un rettangolo da 30x20mm e lo salviamo come dxf
     Rect:=TRect.Create(-15,10,15,-10);
+
+    //Stampiamo un po' di info...
+    WriteLn('Width : ', Rect.Width);
+    WriteLn('Height: ', Rect.Height);
+    WriteLn('Origin: ', Rect.CenterPoint.X,':',Rect.CenterPoint.Y);
+
+    //Fine
+    ReadLn
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
