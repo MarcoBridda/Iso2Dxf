33a940f82bc4e4d0123a7e72b25dce3047a0f06a
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-27 11:16:53 +0100

    Creazione di un rettangolo da 30x20mm centrato sull'origine

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -12,7 +12,8 @@ var
 
 begin
   try
-    //Creiamo un rettangolo e lo salviamo come dxf
+    //Creiamo un rettangolo da 30x20mm e lo salviamo come dxf
+    Rect:=TRect.Create(-15,10,15,-10);
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
