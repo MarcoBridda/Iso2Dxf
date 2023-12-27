885a2743c61e6baacc3a97dd05ca1d3a7b37482d
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-27 16:54:13 +0100

    Aggiunto metodi per inserire elementi (ENTITIES, POLYLINE e VERTEX) nel dxf e chiudere le sequenze

--- Iso2Dxf.DxfClass.pas
+++ Iso2Dxf.DxfClass.pas
@@ -11,10 +11,20 @@ type
   private
     FDxfFile: TstringList;
 
+    //Le informazioni vanno a paio: un codice numerico e un valore associato
+    //ma vanno scritte su due righe separate
     procedure AddItem(const Code: Integer; const Value: String);
   public
     constructor Create;
     destructor Destroy;
+
+    //Metodi per aggiungere elementi specifici
+    procedure BeginEntities();
+    procedure BeginPolyline();
+    procedure AddVertex(const X,Y: Integer);
+    procedure EndPolyline();
+    procedure EndSection();
+    procedure EndOfFile();
   end;
 
 implementation
@@ -27,6 +37,31 @@ begin
   FDxfFile.Add(Value);
 end;
 
+procedure TDxfClass.AddVertex(const X, Y: Integer);
+begin
+  AddItem(0,'POLYLINE');
+  AddItem(8,'0');   //Layer
+  AddItem(10,X.ToString());
+  AddItem(20,Y.ToString());
+  AddItem(30,'0');
+end;
+
+procedure TDxfClass.BeginEntities;
+begin
+  AddItem(0,'SECTION');
+  AddItem(2,'ENTITIES');
+end;
+
+procedure TDxfClass.BeginPolyline;
+begin
+  AddItem(0,'POLYLINE');
+  AddItem(8,'0');   //Layer
+  AddItem(70,'1');  //Polilinea chiusa
+  AddItem(10,'0');
+  AddItem(20,'0');
+  AddItem(30,'0');
+end;
+
 constructor TDxfClass.Create;
 begin
   FDxfFile:=TStringList.Create;
@@ -35,6 +70,21 @@ end;
 destructor TDxfClass.Destroy;
 begin
   FDxfFile.Free;
+end;
+
+procedure TDxfClass.EndOfFile;
+begin
+  AddItem(0,'EOF');
+end;
+
+procedure TDxfClass.EndPolyline;
+begin
+  AddItem(0,'SEQEND');
+end;
+
+procedure TDxfClass.EndSection;
+begin
+  AddItem(0,'ENDSEC');
 end;
 
 end.
