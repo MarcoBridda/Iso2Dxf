9d77b86652da795d3c8b0d1dc89cb59fee370413
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-28 17:40:43 +0100

    Facciamo pulizia per fare esparimenti sull'interpretazione del linguaggio ISO

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -15,32 +15,7 @@ var
 
 begin
   try
-    //Creiamo un rettangolo da 30x20mm e lo salviamo come dxf
-    Rect:=TRect.Create(-15,-10,15,10);
-
-    //Stampiamo un po' di info...
-    WriteLn('Width : ', Rect.Width);
-    WriteLn('Height: ', Rect.Height);
-    WriteLn('Origin: ', Rect.CenterPoint.X,':',Rect.CenterPoint.Y);
-
-    //Creiamo il dxf e scriviamo il rettangolo
-    Dxf:=TDxfClass.Create;
-    try
-      Dxf.BeginEntities();
-      Dxf.BeginPolyline();
-      Dxf.AddVertex(Rect.Left,Rect.Top);
-      Dxf.AddVertex(Rect.Right,Rect.Top);
-      Dxf.AddVertex(Rect.Right,Rect.Bottom);
-      Dxf.AddVertex(Rect.Left,Rect.Bottom);
-      Dxf.EndPolyline();
-      Dxf.EndSection();
-      Dxf.EndOfFile();
-
-      //Scriviamo il file sul disco
-      Dxf.SaveToFile('Prova.dxf');
-    finally
-      Dxf.Free
-    end;
+    
 
     //Fine
     ReadLn
