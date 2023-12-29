a93d67a440465018ef14375615350e040a07fd72
MarcoBridda <marcobridda.mbsoft@gmail.com>
2023-12-29 18:23:25 +0100

    Implementata una funzione per saltare i commenti

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -14,9 +14,31 @@ var
 
 //Normalizza il blocco di comandi ISO: tutto in maiuscolo ed eliminiamo gli
 //e le tabulazioni
-function NormalizeIso(IsoBlock: String): String;
+function NormalizeIso(const IsoBlock: String): String;
 begin
   Result:=IsoBlock.ToUpper().Replace(' ','',[rfReplaceAll]).Replace(#9,'',[rfReplaceAll]);
+end;
+
+//Elimina i commenti dal blocco ISO
+function SkipComment(const IsoBlock: String): String;
+var
+  PosB, PosE: Integer; //Inizio e fine dei commenti
+begin
+  Result:=IsoBlock;
+
+  PosB:=Result.IndexOf('(');
+
+  while PosB>-1 do
+  begin
+    PosE:=Result.IndexOf(')',PosB+1);
+
+    if PosE=-1 then
+      Result:=Result.Remove(PosB)
+    else
+      Result:=Result.Remove(PosB,PosE-PosB+1);
+
+    PosB:=Result.IndexOf('(',PosE+1)
+  end;
 end;
 
 begin
@@ -24,6 +46,7 @@ begin
     repeat
       Write('>');
       ReadLn(IsoLine);
+      IsoLine:=SkipComment(IsoLine);
       IsoLine:=NormalizeIso(IsoLine);
       WriteLn('Hai scritto: ',IsoLine);
     until IsoLine='';
