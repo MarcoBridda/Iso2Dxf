5c730de18a9be9ca444096b6833bfdc85c913b1c
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-01-07 11:21:53 +0100

    Imbastita la struttura per analizzare il file cnc e trasformarlo in un file dxf

--- Iso2Dxf.dpr
+++ Iso2Dxf.dpr
@@ -24,7 +24,9 @@ type
 var
   CncFile: TStringList;
   IsoBlock: TIsoBlock;
+  DxfFile: TDxfFile;
   Line: String;
+  FileName: String;
   W: TIsoWord;
 
 { T3DFloatPoint }
@@ -58,20 +60,30 @@ begin
       try
         CncFile:=TStringList.Create();
         try
-          CncFile.LoadFromFile(TMBCmdline.Param[1]);
-          for Line in CncFile do
-          begin
-            IsoBlock.Block:=Line;
-            if not IsoBlock.IsEmpty then  //Elabora solo se c'è qualcosa
+          FileName:=TMBCmdline.Param[1];
+          CncFile.LoadFromFile(FileName);
+          DxfFile:=TDxfFile.Create;
+          try
+            for Line in CncFile do
             begin
-              WriteLn(Line);
-              WriteLn('{');
-              for W in IsoBlock.Words do
-                WriteLn('  ',W,' - ', W.Address,' = ',W.Value);
-              WriteLn('}');
-              WriteLn;
+              IsoBlock.Block:=Line;
+              if not IsoBlock.IsEmpty then  //Elabora solo se c'è qualcosa
+              begin
+                WriteLn(Line);
+                WriteLn('{');
+                for W in IsoBlock.Words do
+                begin
+                  WriteLn('  ',W,' - ', W.Address,' = ',W.Value);
+                end;
+                WriteLn('}');
+                WriteLn;
+              end;
             end;
+            DxfFile.SaveToFile(ChangeFileExt(FileName,'.dxf'));
+          finally
+            DxfFile.Free;
           end;
+
         finally
           CncFile.Free
         end;
