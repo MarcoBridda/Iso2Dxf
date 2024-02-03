d3ba8c3f805e845cba19a29d533be80388b9a12d
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-02-03 19:04:06 +0100

    Creato una cartella apposita per il progetto e spostato il codice del programma principale e le unit accessorie

---
+++ Iso2Dxf/Iso2Dxf.dpr
@@ -0,0 +1,138 @@
+program Iso2Dxf;
+//****************************************************************************
+//Utility a riga di comando per trasformare un percorso in linguaggio ISO cnc
+//in un DXF.
+//Il file DXF viene generato nello stesso percorso del file ISO.
+//
+//Versione 1.0.0.0
+//
+//Copyright MBSoft(2024)
+//****************************************************************************
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.Math.Vectors,
+  MBSoft.System,
+  MBSoft.System.SysUtils,
+  MBSoft.System.IOUtils,
+  Iso2Dxf.Dxf in 'Source\Iso2Dxf.Dxf.pas',
+  Iso2Dxf.Iso in 'Source\Iso2Dxf.Iso.pas',
+  Iso2Dxf.Utils in 'Source\Iso2Dxf.Utils.pas';
+
+//Una classe di eccezioni personalizzata per il programma principale
+type
+  EIso2DxfMain = class(Exception);
+
+var
+  CncFile: TStringList;
+  IsoBlock: TIsoBlock;
+  DxfFile: TDxfFile;
+  Line: String;
+  FileName: TFileName;
+  W: TIsoWord;
+  Point: TPoint3D;
+  IsMilling: Boolean;
+  Polyline: TPolygon;
+
+//un help che stampa la sintassi di chiamata dell'utility con la possibilità di
+//stampare anche un messaggio di errore personalizzato
+procedure Help(const ErrorMsg: String = '');
+begin
+  WriteLn('Iso2Dxf Versione 1.0.0.0');
+  WriteLn(TCopyInfo.GetLabel(2024));
+  WriteLn;
+  WriteLn('Sintassi: ISO2DXF isofile.cnc');
+  writeLn;
+  writeLn(ErrorMsg)
+end;
+
+{  -- MAIN --  }
+begin
+  try
+    //No parametri o troppi parametri
+    if (not TMBCmdLine.HasParams) or (TMBCmdLine.Count>1) then
+      raise EIso2DxfMain.Create('Indicare il percorso di un file CNC');
+
+    FileName:=TMBCmdLine.Param[1];
+
+    //Percorso non valido
+    if not Filename.Exists then
+      raise EIso2DxfMain.Create('Percorso non valido: ' + FileName);
+
+    //Se tutto va bene proseguiamo
+    CncFile:=TStringList.Create();
+    try
+      CncFile.LoadFromFile(FileName);
+      DxfFile:=TDxfFile.Create;
+      try
+        //Parte iniziale del dxf
+        DxfFile.BeginEntities();
+
+        //Inizializzazione
+        Point:=TPoint3D.Zero;
+        IsMilling:=false;
+
+        IsoBlock:=TIsoBlock.Create();
+        try
+          //Elaborazione
+          for Line in CncFile do
+          begin
+            IsoBlock.Block:=Line;
+            if not IsoBlock.IsEmpty then  //Elabora solo se c'è qualcosa
+            begin
+              WriteLn(Line);
+              WriteLn('{');
+              for W in IsoBlock.Words do
+              begin
+                WriteLn('  ',W,' - ', W.Address,' = ',W.StringValue);
+
+                //Aggiorniamo le posizioni degli assi x, y, z ad ogni blocco
+                case W.Address of
+                  'X': Point.X:=W.FloatValue;
+                  'Y': Point.Y:=W.FloatValue;
+                  'Z': Point.Z:=W.FloatValue
+                end;
+
+              end;
+              WriteLn;
+              WriteLn('  Posizione attuale: ', Point.ToString(W.GetFloatSettings));
+              //Per il momento aggiungiamo tutti i punti senza considerare G0 e G1
+              Polyline.Add(Point.ToPointF);
+              WriteLn('}');
+              WriteLn;
+            end;
+          end;
+          //Inserisci la polilinea che hai trovato
+          DxfFile.AddPolyline(Polyline);
+          //Parte finale del dxf e salvataggio
+          DxfFile.EndSection();
+          DxfFile.EndOfFile();
+          DxfFile.SaveToFile(FileName.ChangeExt('.dxf'))
+        finally
+          IsoBlock.Free
+        end;
+      finally
+        DxfFile.Free
+      end;
+    finally
+      CncFile.Free
+    end;
+
+  except
+    on E: Exception do
+    begin
+      Help(E.Message);
+    {$IFDEF DEBUG}
+      Writeln(E.ClassName, ': ', E.Message);
+    {$ENDIF}
+    end;
+  end;
+  {$IFDEF DEBUG}
+    ReadLn
+  {$ENDIF}
+end.
---
+++ Iso2Dxf/Source/Iso2Dxf.Dxf.pas
@@ -0,0 +1,129 @@
+unit Iso2Dxf.Dxf;
+//****************************************************************************
+//Unit per la parte DXF
+//
+//****************************************************************************
+
+interface
+
+uses
+  System.Classes, System.Types, System.SysUtils, System.Math.Vectors,
+  Iso2Dxf.Utils;
+
+type
+  TDxfFile = class
+  private
+    FLines: TstringList;
+    FFloatSettings: TFloatSettings;
+
+    //Le informazioni vanno a paio: un codice numerico e un valore associato
+    //ma vanno scritte su due righe separate
+    procedure Add(const Code: Integer; const Value: String);
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    //Metodi per aggiungere elementi specifici
+    procedure BeginEntities();
+    procedure BeginPolyline(const IsClosed: Boolean = false);
+    procedure AddVertex(const X,Y: Single);
+    procedure EndPolyline();
+    procedure EndSection();
+    procedure EndOfFile();
+
+    //Aggiunge un' intera polilinea prendendo i punti da un array TPolygon
+    procedure AddPolyline(List : TPolygon);
+
+    //Salva il Dxf sul disco
+    procedure SaveToFile(const FileName: string);
+  end;
+
+implementation
+
+{ TDxfClass }
+
+procedure TDxfFile.Add(const Code: Integer; const Value: String);
+begin
+  FLines.Add('  ' + Code.ToString);
+  FLines.Add(Value);
+end;
+
+procedure TDxfFile.AddPolyline(List: TPolygon);
+var
+  Vertex: TPointF;
+begin
+  if List.IsPolyline then
+  begin
+    BeginPolyline();
+
+    for Vertex in List do
+      AddVertex(Vertex.X, Vertex.Y);
+
+    EndPolyline();
+  end;
+end;
+
+procedure TDxfFile.AddVertex(const X, Y: Single);
+begin
+  Add(0,'VERTEX');
+  Add(8,'0');   //Layer
+  Add(10,FFloatSettings.FloatToStr(X));
+  Add(20,FFloatSettings.FloatToStr(Y));
+  Add(30,FFloatSettings.FloatToStr(0));
+end;
+
+procedure TDxfFile.BeginEntities;
+begin
+  Add(0,'SECTION');
+  Add(2,'ENTITIES');
+end;
+
+procedure TDxfFile.BeginPolyline(const IsClosed: Boolean);
+begin
+  Add(0,'POLYLINE');
+  Add(8,'0');     //Layer
+
+  if IsClosed then
+    Add(70,'1');  //Polilinea chiusa
+
+  Add(10,'0');
+  Add(20,'0');
+  Add(30,'0');
+end;
+
+constructor TDxfFile.Create;
+begin
+  FLines:=TStringList.Create;
+
+  //Inizializza alle impostazioni di default
+  FFloatSettings:=TFloatSettings.Default
+end;
+
+destructor TDxfFile.Destroy;
+begin
+  FLines.Free;
+
+  inherited;
+end;
+
+procedure TDxfFile.EndOfFile;
+begin
+  Add(0,'EOF');
+end;
+
+procedure TDxfFile.EndPolyline;
+begin
+  Add(0,'SEQEND');
+end;
+
+procedure TDxfFile.EndSection;
+begin
+  Add(0,'ENDSEC');
+end;
+
+procedure TDxfFile.SaveToFile(const FileName: string);
+begin
+  FLines.SaveToFile(FileName);
+end;
+
+end.
