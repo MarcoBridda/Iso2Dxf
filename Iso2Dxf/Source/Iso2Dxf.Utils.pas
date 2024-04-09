0c908175f177aca04954da4606652d437b95b752
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-04-09 16:34:37 +0200

    Spostato TPolygonHelper nella unit MBSoft.System.Math.Vectors

--- Iso2Dxf/Source/Iso2Dxf.Utils.pas
+++ Iso2Dxf/Source/Iso2Dxf.Utils.pas
@@ -42,21 +42,6 @@ interface
     function ToPointF: TPointF;
   end;
 
-  //Un record helper per il tipo TPolygon per aggiungere e togliere punti
-  TPolygonHelper = record helper for TPolygon
-    //Pulisce l'array eliminado tutti i punti
-    procedure Clear;
-
-    //Aggiunge un punto alla fine del poligono solo se non è uguale al precedente
-    procedure Add(const Point: TPointF);
-
-    //Vera se la variabile non contiene elementi
-    function IsEmpty: Boolean;
-
-    //Vera se la variabile contiene almeno 2 elementi (almeno una linea)
-    function IsPolyline: Boolean;
-  end;
-
   //Una struttura statica per ricavare le info dell'app
   TAppInfo = record
   private
@@ -154,38 +139,6 @@ end;
 function TPoint3DHelper.ToString(const Pattern: String): String;
 begin
   Result:=Self.ToString(TFloatSettings.Default, Pattern);
-end;
-
-{ TPolygonHelper }
-
-procedure TPolygonHelper.Add(const Point: TPointF);
-var
-  MaxI: Integer;
-begin
-  MaxI:=High(Self);
-
-  if (Length(Self)=0) or (Self[MaxI]<>Point) then
-  begin
-    Inc(MaxI);
-    SetLength(Self,MaxI+1);
-    Self[MaxI]:=Point
-  end;
-
-end;
-
-procedure TPolygonHelper.Clear;
-begin
-  SetLength(Self,0);
-end;
-
-function TPolygonHelper.IsEmpty: Boolean;
-begin
-  Result:=Length(Self)=0
-end;
-
-function TPolygonHelper.IsPolyline: Boolean;
-begin
-  Result:=Length(Self)>=2;
 end;
 
 { TAppInfo }
