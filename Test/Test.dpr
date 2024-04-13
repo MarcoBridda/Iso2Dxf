dc1c972378e9bf2ed467540fdfd5fd373858ec17
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-04-13 18:16:21 +0200

    Eseguito test sul tipo TPoligonList definito nella unit MBSoft.System.Math.Vectors

--- Test/Test.dpr
+++ Test/Test.dpr
@@ -12,6 +12,7 @@ uses
 
 var
   P1,P2: TPolygon;
+  List: TPolygonList;
 
 begin
   try
@@ -19,38 +20,18 @@ begin
     P1.Add(PointF(10,20));
     P1.Add(PointF(20,20));
 
-    WriteLn('Length  : ', Length(P1));
-    WriteLn('IsOpened: ', P1.IsOpened);
-    WriteLn('IsClosed: ', P1.IsClosed);
-
-    P1.Close;
-    WriteLn;
-
-    WriteLn('Length  : ', Length(P1));
-    WriteLn('IsOpened: ', P1.IsOpened);
-    WriteLn('IsClosed: ', P1.IsClosed);
-
-    P2:=Copy(P1,low(P1),Length(P1));
-    WriteLn;
-
-    WriteLn('IsEqual : ', P1.IsEqual(P2));
-
-    P2[3]:=PointF(40,40);
-    WriteLn;
-
-    WriteLn('IsEqual : ', P1.IsEqual(P2));
-    WriteLn('Length  : ', Length(P2));
-    WriteLn('IsOpened: ', P2.IsOpened);
-    WriteLn('IsClosed: ', P2.IsClosed);
-
+    P2:=Copy(P1,Low(P1),Length(P1));
     P2.Close;
-    P2.Add(PointF(20,20));
-    WriteLn;
 
-    WriteLn('IsEqual : ', P1.IsEqual(P2));
-    WriteLn('Length  : ', Length(P2));
-    WriteLn('IsOpened: ', P2.IsOpened);
-    WriteLn('IsClosed: ', P2.IsClosed);
+    List:=TPolygonList.Create;
+    try
+      List.Add(P1);
+      List.Add(P2);
+    finally
+      List.Free
+    end;
+
+    WriteLn;
 
     ReadLn
   except
